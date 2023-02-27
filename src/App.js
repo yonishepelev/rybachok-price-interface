797372c263762601d42b3173891e213d8508f083
Ivan Shepelev <yonishepelev@gmail.com>
2023-02-27 14:54:35 +0200

    кнопка готова

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "./components/Button/Button";
 
 function App() {
+  const textButton = "Изменение цены для отмеченных товаров";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        text={textButton}
+        onClickCallback={() => {
+          console.log("Я колбек оранжевой кнопки");
+        }}
+        color="orange"
+      />
+      <Button text={"Рассчитать"} color="brightCyan" />
     </div>
   );
 }
